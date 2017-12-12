1db9cde82a8a1c2c20d7ba1975cb736eb12f641f
n2omatt <n2omatt@amazingcow.com>
2017-12-12 17:53:50 -0200

    - Add new Configurations.

--- include/Config.h
+++ include/Config.h
@@ -1,17 +1,26 @@
 #pragma once
 
 //----------------------------------------------------------------------------//
+// Debug Configs                                                              //
+//----------------------------------------------------------------------------//
+#define COREASSERT_CONFIG_MODE_DEBUG   1
+#define COREASSERT_CONFIG_MODE_RELEASE 0
+
+#define COREASSERT_CONFIG_MODE COREASSERT_CONFIG_MODE_DEBUG
+
+
+//----------------------------------------------------------------------------//
 // Assert Configs                                                             //
 //----------------------------------------------------------------------------//
-#define COREASSERT_CONFIG_ENABLE_ASSERTS 1
+#define COREASSERT_CONFIG_ENABLE_ASSERTS COREASSERT_CONFIG_MODE
 
 
 //----------------------------------------------------------------------------//
 // Exceptions Configs                                                         //
 //----------------------------------------------------------------------------//
-#define COREASSERT_CONFIG_ENABLE_EXCEPTIONS         1
+#define COREASSERT_CONFIG_ENABLE_EXCEPTIONS         0
 #define COREASSERT_CONFIG_ASSERT_INSTEAD_EXCEPTIONS 0
-#define COREASSERT_CONFIG_VERIFY_INSTEAD_EXCEPTIONS 0
+#define COREASSERT_CONFIG_VERIFY_INSTEAD_EXCEPTIONS 1
 
 #if !COREASSERT_CONFIG_ENABLE_EXCEPTIONS && (COREASSERT_CONFIG_ASSERT_INSTEAD_EXCEPTIONS == COREASSERT_CONFIG_VERIFY_INSTEAD_EXCEPTIONS)
     #error "Cannot have both Assert and Verify instead Exception."
